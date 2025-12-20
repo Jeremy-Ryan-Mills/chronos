0c104a43c763d3ce1ecae1eb1c43e74fb499727e
Jeremy Mills <jeremymills496@gmail.com>
2025-12-19 23:56:37 -0800

    Implemented VGA Buffer

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World!";
+mod vga_buffer;
 
 
 #[unsafe(no_mangle)] // Don't mangle the name of this function
@@ -14,20 +14,13 @@ pub extern "C" fn _start() -> ! {
      * the linker looks for a function named
      * _start
      */
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    println!("Hello World{}", "!");
     loop {}
 }
 
 // Called on panic
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
