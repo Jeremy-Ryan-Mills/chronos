033f38c505ad004bc862e1b6202b04b49e9898a7
Jeremy Mills <jeremymills496@gmail.com>
2025-12-29 08:53:58 -0800

    Starting to implement the bump allocator

--- src/main.rs
+++ src/main.rs
@@ -4,33 +4,50 @@
 #![test_runner(chronos::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+extern crate alloc;
+use alloc::{boxed::Box, vec, vec::Vec, rc::Rc};
+
 use bootloader::{BootInfo, entry_point};
 use chronos::println;
+use x86_64::VirtAddr;
 use core::panic::PanicInfo;
 
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use x86_64::{structures::paging::Page, VirtAddr};
-    use chronos::memory::BootInfoFrameAllocator;
-    use chronos::memory;
-    
+
     println!("Hello World{}", "!");
     chronos::init();
+
+    use chronos::allocator;
+    use chronos::memory::{self, BootInfoFrameAllocator};
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
     let mut frame_allocator = unsafe {
         BootInfoFrameAllocator::init(&boot_info.memory_map)
     };
-    // map an unused page
-    let page = Page::containing_address(VirtAddr::new(0));
-    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
-    // write the string `New!` to the screen through the new mapping
-    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
-    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
+    allocator::init_heap(&mut mapper, &mut frame_allocator)
+        .expect("heap initialization failed");
 
+    // allocate a number on the heap
+    let heap_value = Box::new(41);
+    println!("heap_value at {:p}", heap_value);
+
+    // create a dynamically sized vector
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
+    }
+    println!("vec at {:p}", vec.as_slice());
+
+    // create a reference counted vector -> will be freed when count reaches 0
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+    println!("current reference count is {}", Rc::strong_count(&cloned_reference));
+    core::mem::drop(reference_counted);
+    println!("reference count is {} now", Rc::strong_count(&cloned_reference));
     // For testing
     #[cfg(test)]
     test_main();
